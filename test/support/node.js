6eba48359016a9f159745eb889687365c54dd614
sebv <seb.vincent@gmail.com>
2013-10-24 23:16:16 +0800

    Add configurable promise enricher.

--- test/support/node.js
+++ test/support/node.js
@@ -7,6 +7,18 @@ var Q = require("q");
 chai.should();
 chai.use(chaiAsPromised);
 
+if (process.env.PROMISIFY_WITH === "Q") {
+    chai.promisifyWith("Q");
+}
+
+if (process.env.PROMISIFY_WITH === "CUSTOM") {
+    chai.promisifyWith(function (that, derivedPromise) {
+        chai.promisifyMethods.default(that, derivedPromise);
+        that.done = derivedPromise.done.bind(derivedPromise);
+        that.fin = derivedPromise.fin.bind(derivedPromise);
+    });
+}
+
 global.expect = chai.expect;
 global.AssertionError = chai.AssertionError;
 global.Assertion = chai.Assertion;
