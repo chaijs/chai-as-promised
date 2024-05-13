0335b677348c2ba253c114e8b0828bcc67819c40
James Garbutt <43081j@users.noreply.github.com>
2024-05-13 20:31:42 +0100

    chore: add prettier (#286)
    Adds prettier and the associated formatting changes.
    
    This also updates the editor config to match that of chai itself.

--- test/configurable-promiseness-transfer.js
+++ test/configurable-promiseness-transfer.js
@@ -1,22 +1,22 @@
-"use strict";
-require("./support/setup.js");
-const chaiAsPromised = require("..");
-const originalTransferPromiseness = require("..").transferPromiseness;
+'use strict';
+require('./support/setup.js');
+const chaiAsPromised = require('..');
+const originalTransferPromiseness = require('..').transferPromiseness;
 
-describe("Configuring the way in which promise-ness is transferred", () => {
-    afterEach(() => {
-        chaiAsPromised.transferPromiseness = originalTransferPromiseness;
-    });
+describe('Configuring the way in which promise-ness is transferred', () => {
+  afterEach(() => {
+    chaiAsPromised.transferPromiseness = originalTransferPromiseness;
+  });
 
-    it("should return a promise with the custom modifications applied", () => {
-        chaiAsPromised.transferPromiseness = (assertion, promise) => {
-            assertion.then = promise.then.bind(promise);
-            assertion.isCustomized = true;
-        };
+  it('should return a promise with the custom modifications applied', () => {
+    chaiAsPromised.transferPromiseness = (assertion, promise) => {
+      assertion.then = promise.then.bind(promise);
+      assertion.isCustomized = true;
+    };
 
-        const promise = Promise.resolve("1234");
-        const assertion = promise.should.become("1234");
+    const promise = Promise.resolve('1234');
+    const assertion = promise.should.become('1234');
 
-        assertion.should.have.property("isCustomized", true);
-    });
+    assertion.should.have.property('isCustomized', true);
+  });
 });
--- test/support/setup.js
+++ test/support/setup.js
@@ -1,12 +1,12 @@
-const chai = require("chai");
-const chaiAsPromised = require("../../lib/chai-as-promised.js");
+const chai = require('chai');
+const chaiAsPromised = require('../../lib/chai-as-promised.js');
 
 chai.should();
 chai.use(chaiAsPromised);
 
-process.on("unhandledRejection", () => {
-    // Do nothing; we test these all the time.
+process.on('unhandledRejection', () => {
+  // Do nothing; we test these all the time.
 });
-process.on("rejectionHandled", () => {
-    // Do nothing; we test these all the time.
+process.on('rejectionHandled', () => {
+  // Do nothing; we test these all the time.
 });
